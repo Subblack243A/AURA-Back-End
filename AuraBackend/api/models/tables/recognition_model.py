f2b192d8b1151950e08d2cffb771e1b83d743722
Subblack <osfel15102003@gmail.com>
2025-10-13 17:52:33 -0500

    fix: FK's models names

--- AuraBackend/api/models/tables/recognition_model.py
+++ AuraBackend/api/models/tables/recognition_model.py
@@ -3,5 +3,5 @@ from django.db import models
 class RecognitionModel(models.Model):
     ID_Recognition = models.AutoField(primary_key=True, unique=True)
     RecognitionResults = models.CharField(max_length=255)
-    FK_User = models.ForeignKey('User', on_delete=models.CASCADE)
+    FK_User = models.ForeignKey('UserModel', on_delete=models.CASCADE)
     dateOfRecognition = models.DateTimeField(auto_now_add=True)
--- AuraBackend/api/models/tables/survey_model.py
+++ AuraBackend/api/models/tables/survey_model.py
@@ -3,5 +3,5 @@ from django.db import models
 class SurveyModel(models.Model):
     ID_Survey = models.AutoField(primary_key=True)
     FK_User = models.ForeignKey('User', on_delete=models.CASCADE)
-    FK_SurveyResult = models.ForeignKey('SurveyResult', on_delete=models.CASCADE)
+    FK_SurveyResult = models.ForeignKey('SurveyResultModel', on_delete=models.CASCADE)
     SurveyuDate = models.DateTimeField(auto_now_add=True)
