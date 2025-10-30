edd8a59b38fb4de057c1de26b4bb4c29556fd5aa
Subblack <osfel15102003@gmail.com>
2025-10-29 20:29:09 -0500

    refactor: Implements pgvector for embeddings in database

--- AuraBackend/api/models/tables/user_model.py
+++ AuraBackend/api/models/tables/user_model.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from pgvector.django import VectorField
 
 # Control de autentifiación y autorización de usuarios
 class UserModel(AbstractUser):
@@ -9,7 +10,7 @@ class UserModel(AbstractUser):
     FK_Program = models.ForeignKey('DictionaryProgramModel', on_delete=models.CASCADE)
     FK_Faculty = models.ForeignKey('DictionaryFacultyModel', on_delete=models.CASCADE)
     DateOfBirth = models.DateField()
-    Face = models.CharField(max_length=255, null=True)
+    Face = VectorField(dimensions=512, null=True, blank=True)
     FK_HealthcareProfessional = models.ForeignKey('UserModel', on_delete=models.CASCADE, null=True)
     DataAuth = models.BooleanField(default=False)
     Semester = models.IntegerField()
