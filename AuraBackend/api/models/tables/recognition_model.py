0916c8aae3a6c8f4ea562d92fe139cc69f425ecd
Subblack <osfel15102003@gmail.com>
2025-11-04 20:23:44 -0500

    refactor: Changes to improve database fields

--- AuraBackend/api/models/tables/recognition_model.py
+++ AuraBackend/api/models/tables/recognition_model.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class RecognitionModel(models.Model):
     ID_Recognition = models.AutoField(primary_key=True, unique=True)
-    RecognitionResults = models.CharField(max_length=255)
+    RecognitionResults = models.JSONField(default=dict, blank=True)
     FK_User = models.ForeignKey('UserModel', on_delete=models.CASCADE)
     dateOfRecognition = models.DateTimeField(auto_now_add=True)
