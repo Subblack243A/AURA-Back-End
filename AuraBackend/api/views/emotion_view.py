028f7f9aa61668ab85a48c825ab485da32ccdced
subblack <osfel15102003@gmail.com>
2026-03-07 14:59:25 -0500

    refactor: emotion time endpoint

--- AuraBackend/api/views/emotion_view.py
+++ AuraBackend/api/views/emotion_view.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -15,4 +16,16 @@ class EmotionRegisterView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        return Response({"message": "Emotion registered successfully"}, status=status.HTTP_201_CREATED) 
+        return Response({"message": "Emotion registered successfully"}, status=status.HTTP_201_CREATED)
+
+    def get(self, request, *args, **kwargs):
+        """
+        Retorna la fecha de los últimos registros de emoción del usuario para control de ráfagas.
+        """
+        from api.models.tables.emotion_register_model import EmotionRegisterModel
+        last_regs = EmotionRegisterModel.objects.filter(FK_User=request.user).order_by('-EmotionDate')[:3]
+        
+        return Response({
+            "last_registrations": [reg.EmotionDate for reg in last_regs],
+            "server_time": timezone.now()
+        }, status=status.HTTP_200_OK)
