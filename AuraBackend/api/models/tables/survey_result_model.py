b8ae14747d7b91745faa8f09035473e7af7e1c71
subblack <osfel15102003@gmail.com>
2026-03-08 17:06:28 -0500

    refactor: survey model

--- AuraBackend/api/models/tables/survey_result_model.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class SurveyResultModel(models.Model):
-    ID_SurveyResult = models.AutoField(primary_key=True)
-    Result = models.CharField(unique=True, max_length=100)
